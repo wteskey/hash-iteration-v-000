7a1540f6b0736505813a2dd5cdf5ca92d20fbe81
William J Teskey <willjteskey@gmail.com>
2018-04-16 23:00:51 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,4 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+
 
 def select_winner(passengers)
  winner = ""
